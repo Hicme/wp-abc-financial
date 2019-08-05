d5cce1ab20df78c8805378ab25b3ab1333cba21a
Oleg Podmarkov <oleg.podmarkov@prosvit.design>
2019-08-05 16:56:58 +0300

    updated g block

--- includes/system/Carbon.php
+++ includes/system/Carbon.php
@@ -1,57 +1,75 @@
 <?php
-
 namespace system;
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
-
 class Carbon
 {
-
-    public function __construct()
-    {
-      add_action( 'carbon_fields_register_fields', [ $this, 'load_fields' ] );
+  public function __construct()
+  {
+    add_action('carbon_fields_register_fields', [$this, 'load_fields']);
+  }
+  public function load_fields()
+  {
+    Block::make('Page Content')
+      ->add_fields(array(
+        Field::make('separator', 'hh_separator', 'Page Content'),
+        Field::make( 'media_gallery', 'gallery', __( 'Images', 'wpabcf' ) )
+          ->set_type( [ 'image' ] ),
+        Field::make( 'complex', 'repeter', __( 'Blocks', 'wpabcf' ) )
+          ->add_fields([
+            Field::make( 'text', 'title', __( 'Block Title', 'wpabcf' ) ),
+            Field::make( 'rich_text', 'content', __( 'Block Content', 'wpabcf' ) )
+          ])
+      ))
+      ->set_category('ABC Financial')
+      ->set_render_callback([__CLASS__, 'render_section']);
+  }
+  public static function render_section($fields, $attributes, $content)
+  {
+    $section_class = array('section', 'section-hero section--fullscren');
+    if (isset($attributes['className'])) {
+      array_push($section_class, $attributes['className']);
     }
 
-    public function load_fields()
-    {
-
-      Block::make('Hero Section')
-        ->add_fields(array(
-            Field::make('separator', 'hh_separator', 'Home Hero'),
-            Field::make('rich_text', 'hh_content', 'Hero Content'),
-            Field::make('image', 'hh_background_image', 'Background Image')
-                ->set_value_type('id')
-                ->set_type('image'),
-        ))
-        ->set_category('ABC Financial')
-        ->set_render_callback( [ __CLASS__, 'render_hero_section' ] );
-
-    }
-
-    public static function render_hero_section( $fields, $attributes, $content )
-    {
-      $section_class = array('section', 'section-hero section--fullscren');
-      if (isset($attributes['className'])) {
-          array_push($section_class, $attributes['className']);
-      }
+    if( $fields['gallery'] ) {
       ?>
-      <section class="<?php echo implode(" ", $section_class) ?>">
-
-          <?php if (isset($fields['hh_background_image'])) { ?>
-              <div class="section__background">
-                  <?php echo wp_get_attachment_image($fields['hh_background_image'], 'background-half', "", array("class" => "img-fluid section__backgroundimage"));  ?>
-              </div>
-          <?php } ?>
-
-          <div class="container">
-              <?php
-              if (!empty($fields['hh_content'])) {
-                  echo $fields['hh_content'];
-              }
+      <div class="section--col col-2">
+        <ul class="image-gallery row">
+          <?php
+            foreach( $fields['gallery'] as $image ){
               ?>
-          </div>
-      </section>
+                <li class="image-gallery__item col-2">
+                  <?php echo wp_get_attachment_image( $image, 'medium' ); ?>
+                </li>
+              <?php
+            }
+          ?>
+        </ul>
+      </div>
       <?php
     }
+    ?>
 
+    <div class="section--col col-2">
+      <div class="overflow-text">
+        <?php
+          if( $fields['repeter'] ){
+            $total = count( $fields['repeter'] );
+            foreach( $fields['repeter'] as $item => $block ){
+              ?>
+              <h3>
+                <?php echo $block['title']; ?>
+              </h3>
+              <?php
+                echo $block['content'];
+                if( $item > 0 && $item < $total ){
+                  echo '<hr>';
+                }
+            }
+          }
+        ?>
+      </div>
+    </div>
+  <?php
+  }
 }
